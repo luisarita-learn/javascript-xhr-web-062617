392593f6c373670ad0f80f126ea3e3018b10aa17
luisarita <github@luisarita.com>
2017-08-15 09:33:45 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function getCommits(el) {
+  const name = el.dataset.repo
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showCommits)
+  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
+  req.send()
+}
+
+function showCommits() {
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("commits").innerHTML = commitsList
+}
